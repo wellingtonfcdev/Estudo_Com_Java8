b229219013cbe372743602068a230b4d9f22d8d0
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-07-09 09:28:14 -0300

    Criação da classe EmpresaRepository

---
+++ P16/src/main/java/com/wellingtondev/api/repositories/EmpresaRepository.java
@@ -0,0 +1,10 @@
+package com.wellingtondev.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wellingtondev.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+
+	Empresa findByCnpj(String cnpj);
+}
