80e9aa568db1a3564463b7c2a91b9251b546a0a6
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-07-09 09:14:39 -0300

    projeto repositorios jpa

---
+++ P16/src/main/java/com/wellingtondev/api/P16Application.java
@@ -0,0 +1,13 @@
+package com.wellingtondev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P16Application.class, args);
+	}
+
+}
---
+++ P16/src/test/java/com/wellingtondev/api/P16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellingtondev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
