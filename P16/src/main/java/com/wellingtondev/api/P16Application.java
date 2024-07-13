069b2f0f6af34aecd24ef7357f43bf1866353b74
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-07-13 11:48:06 -0300

    Classe ajustes realizados

--- P16/src/main/java/com/wellingtondev/api/P16Application.java
+++ P16/src/main/java/com/wellingtondev/api/P16Application.java
@@ -1,13 +1,54 @@
 package com.wellingtondev.api;
 
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.wellingtondev.api.entities.Empresa;
+import com.wellingtondev.api.repositories.EmpresaRepository;
+
 
 @SpringBootApplication
 public class P16Application {
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(P16Application.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Wellingtondev");
+			empresa.setCnpj("63228837000102");
+			
+			this.empresaRepository.save(empresa);
+			
+			List<Empresa> empresas = empresaRepository.findAll();
+			empresas.forEach(System.out::println);
+			
+			Empresa empresaDb = empresaRepository.findOne(1L);
+			System.out.println("Empresa por ID: " + empresaDb);
+			
+			empresaDb.setRazaoSocial("Kazale IT Web");
+			this.empresaRepository.save(empresaDb);
+
+			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
+			System.out.println("Empresa por CNPJ: " + empresaCnpj);
+			
+			this.empresaRepository.delete(1L);
+			empresas = empresaRepository.findAll();
+			System.out.println("Empresas: " + empresas.size());
+			};
 	}
 
 }
