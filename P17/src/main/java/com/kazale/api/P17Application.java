d1e48f01d0ca3cff33e070a881ba1c600a7d05f3
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-07-13 11:51:03 -0300

    Projeto 17

---
+++ P17/src/main/java/com/kazale/api/P17Application.java
@@ -0,0 +1,12 @@
+package com.kazale.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P17Application.class, args);
+	}
+}
